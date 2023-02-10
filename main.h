4decfff1dc62625d30f39a99498c128ce8c19b99
Harry Forshaw SE2021 <harry.forshaw.2020@uni.strath.ac.uk>
2023-02-10 14:43:37 +0000

    Header file including stage 1 functions

---
+++ main.h
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+//Find the user home directory from the environment
+//Set current working directory to user home directory
+//Save the current path
+//Load history
+//Load aliases
+void loop_shell();
+	void display();
+	void read_parse();
+//	While the command is a history invocation or alias then replace it with the
+//	appropriate command from history or the aliased command respectively
+//	If command is built-in invoke appropriate function
+//	Else execute command as an external process
+//End while
+//Save history
+//Save aliases
+//Restore original path
+//Exit
