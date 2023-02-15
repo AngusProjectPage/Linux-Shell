1948c433dc796383be6f8d0a0781fe7cc0784168
Angus <angus.duffy.2021@uni.strath.ac.uk>
2023-02-15 13:43:50 +0000

    Stage2 Updated with segmentation fault

--- stage2.c
+++ stage2.c
@@ -9,24 +9,19 @@
 #define BUFFER_SIZE 512
 char buffer[BUFFER_SIZE], *token;
 int main(int argc, char **argv) {
-    char ** tokens;
-    pid_t pid;
-    pid = fork();
-    if(pid < 0) {
-        fprintf(stderr, "Fork Failed");
-        return 1;
-    }
-    else if (pid == 0) { /* Child Process */
-         tokens = read_parse();  
-        execvp(tokens[0],tokens);
-    }
-    else { /* parent process */
-        /* parent will wait for the child process to complete*/
-        wait(NULL);
-        printf("Child complete");
-    }
     loop_shell();
     return 0;
+}
+
+void loop_shell() {
+    do{
+        display();
+        start_fork();
+        if(feof(stdin)){
+            break;
+        }
+
+    } while(strcmp(buffer, "quit\n") != 0 );
 }
 
 void display(){
@@ -35,27 +30,32 @@ void display(){
     printf("%s>>> ", cwd);
 }
 
-
-
-void loop_shell() {
-
-    do{
-        display();
-        read_parse();
-        if(feof(stdin)){
-            break;
-        }
-
-    } while(strcmp(buffer, "quit\n") != 0 );
+int start_fork() {
+    char *arguments[50];
+    read_parse(arguments); 
+    pid_t pid;
+    pid = fork();
+    if(pid < 0) {
+        fprintf(stderr, "Fork Failed");
+        return 1;
+    }
+    else if (pid == 0) { /* Child Process */ 
+        execvp(arguments[0],arguments);
+    }
+    else { /* parent process */
+        /* parent will wait for the child process to complete*/
+        wait(NULL);
+        printf("Child complete");
+        return 0;
+    }
 }
 
-char** read_parse(){
+void read_parse(char** arguments){
    
     char delim[] = " "; // Each token to be split by whitespace 
     char *c;
-    char * t;
+    char *t;
     int i = 0;
-
         t = fgets(buffer, BUFFER_SIZE, stdin);
         if(buffer[strlen(buffer) - 1] != '\n' && !feof(stdin)) {
             char c;
@@ -63,15 +63,15 @@ char** read_parse(){
             printf("input should not exceed 512 characters, try again\n");
         }
         else {
-                char*arguments[50];
-                *token = strtok(buffer, delim);
-                while(*token != NULL) {
-                    *token = strtok(NULL, delim);
+                char *token = strtok(buffer, delim);
+                while(token != NULL) {
+                    token = strtok(NULL, delim);
+                    arguments[i] = malloc(strlen(token) + 1);
                     arguments[i] = token;
                     i++;
-                    }
-                return arguments;
+                }
+                arguments[i] = NULL;
             } 
-        }
+    }
     
 
